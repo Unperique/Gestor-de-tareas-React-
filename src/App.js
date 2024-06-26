a76be481e2e965210e904f74621c9adf8b088c46
Un perique <andresfelipemoralesmejia@gmail.com>
2024-06-25 20:02:38 -0500

    Arreglo

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="aplicacio-tareas"> {/* Div principal con la clase aplicacio-tareas */}
       <div className='freecodecamp-logo-contenedor'> {/* Div con la clase freecodecamp-logo-contenedor */}
-        <img  {/* Imagen con la clase freecodecamp-logo */}
-          src={freeCodeCampLogo} {/* Atributo src con la ruta de la imagen freeCodeCampLogo */}
+        <img  /* Imagen con la clase freecodecamp-logo */
+          src={freeCodeCampLogo} /* Atributo src con la ruta de la imagen freeCodeCampLogo */
           className='freecodecamp-logo' /> {/* Atributo className con la clase freecodecamp-logo */}
       </div>
       <div className='tareas-lista-principal'> {/* Div con la clase tareas-lista-principal */}
